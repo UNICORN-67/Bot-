c38510832d9ba72261b5a891595b0308e8cd1fb6
UNICORN-67 <parthtsingh@gmail.com>
2025-04-09 15:29:28 +0530

    Update stats.py

--- plugins/general/stats.py
+++ plugins/general/stats.py
@@ -1,1 +1,34 @@
-# Optional group usage stats
+import platform
+import psutil
+import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from languages.get import get_string
+from utils.uptime import get_uptime
+from redis.redisdb import redis
+
+lang = get_string("en")
+
+@Client.on_message(filters.command("stats"))
+async def stats_command(client: Client, message: Message):
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+    os_info = f"{platform.system()} {platform.release()}"
+    uptime = get_uptime()
+
+    try:
+        ping_start = time.time()
+        await redis.ping()
+        redis_ping = round((time.time() - ping_start) * 1000, 2)
+    except:
+        redis_ping = "ᴏғғʟɪɴᴇ"
+
+    stats_text = (
+        f"**{lang['general']['stats']}**\n"
+        f"ᴏꜱ: {os_info}\n"
+        f"ᴜᴘᴛɪᴍᴇ: {uptime}\n"
+        f"ᴄᴘᴜ: {cpu}%\n"
+        f"ʀᴀᴍ: {ram}%\n"
+        f"ʀᴇᴅɪꜱ: {redis_ping} ms"
+    )
+    await message.reply(stats_text)
