e95aa42f81d6d0718086f1fc73c70378c83ad300
UNICORN-67 <parthtsingh@gmail.com>
2025-04-09 15:37:02 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,12 @@
 # Logger
+
+from pyrogram import Client
+from config import LOG_CHANNEL_ID
+
+async def log_to_channel(client: Client, text: str):
+    if not LOG_CHANNEL_ID:
+        return
+    try:
+        await client.send_message(LOG_CHANNEL_ID, text)
+    except Exception as e:
+        print(f"Logging failed: {e}")
