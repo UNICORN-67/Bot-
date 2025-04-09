5462ed939142519b582bb349025eda0c876fe0ed
UNICORN-67 <parthtsingh@gmail.com>
2025-04-09 15:35:32 +0530

    Update fonts.py

--- utils/fonts.py
+++ utils/fonts.py
@@ -1,1 +1,16 @@
 # Font converter
+
+import yaml
+
+with open("languages/font.yml", "r", encoding="utf-8") as f:
+    font_data = yaml.safe_load(f)
+
+def stylize(text: str, style: str = "smallcaps") -> str:
+    if style not in font_data:
+        return text  # Fallback
+
+    mapping = font_data[style]
+    styled_text = ""
+    for char in text:
+        styled_text += mapping.get(char, char)
+    return styled_text
