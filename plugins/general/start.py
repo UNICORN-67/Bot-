ee6bf255de15b9d5fba2420678b44b7e760c135b
UNICORN-67 <parthtsingh@gmail.com>
2025-04-09 15:27:47 +0530

    Update start.py

--- plugins/general/start.py
+++ plugins/general/start.py
@@ -1,1 +1,12 @@
 # Start and help command
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from languages.get import get_string
+
+lang = get_string("en")  # You can modify this to be dynamic per user/group
+
+@Client.on_message(filters.command("start"))
+async def start_command(client: Client, message: Message):
+    text = lang["general"]["start_text"]
+    await message.reply(text)
