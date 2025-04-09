e73e76ba6baa356a222c982d8f20c1eb0bdd32cd
UNICORN-67 <parthtsingh@gmail.com>
2025-04-09 23:56:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,7 +12,7 @@ API_HASH = os.getenv("API_HASH", "c426d9f7087744afdafc961a620b6338")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8146545438:AAFoq4cmKkhnkNAz9x65-8F_HxeeSy-hWBA")
 
 # Redis Configuration
-REDIS_URI = os.getenv("REDIS_URI", "redis-cli -u redis://default:pR2c50esNfwPGODo0gn7Zvokbxwdg0t1@redis-17260.c73.us-east-1-2.ec2.redns.redis-cloud.com:17260")
+REDIS_URI = os.getenv("REDIS_URI", "redis://default:pR2c50esNfwPGODo0gn7Zvokbxwdg0t1@redis-17260.c73.us-east-1-2.ec2.redns.redis-cloud.com:17260")
 
 # Logging and Permissions
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", -1002621770250))  # set to 0 if not used
