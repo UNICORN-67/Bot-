ed33cc91d627109a3ece95073066e33183d4b69a
UNICORN-67 <parthtsingh@gmail.com>
2025-04-09 18:56:59 +0530

    Update redisdb.py

--- redis/redisdb.py
+++ redis/redisdb.py
@@ -4,3 +4,9 @@ from redis import from_url
 from bot import config
 
 redis = from_url(config.REDIS_URI, decode_responses=True)
+
+async def get_toggle(toggle_type: str, chat_id: int) -> bool:
+    return r.get(f"{toggle_type}_{chat_id}") == "true"
+
+async def set_toggle(toggle_type: str, chat_id: int, value: bool):
+    r.set(f"{toggle_type}_{chat_id}", str(value).lower())
