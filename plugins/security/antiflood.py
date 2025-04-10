c2149d5a29ac50901e4232d3efe40d349785fdca
UNICORN-67 <parthtsingh@gmail.com>
2025-04-10 14:02:10 +0530

    Update antiflood.py

--- plugins/security/antiflood.py
+++ plugins/security/antiflood.py
@@ -2,7 +2,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from redis.redisdb import redis
+from db.redisdb import redis
 from languages.get import get_string
 import time
 
