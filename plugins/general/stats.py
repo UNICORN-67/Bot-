3339ae9cf99b2ff0cfc0cd69547b4ac688089290
UNICORN-67 <parthtsingh@gmail.com>
2025-04-10 20:10:53 +0530

    Update stats.py

--- plugins/general/stats.py
+++ plugins/general/stats.py
@@ -1,34 +1,16 @@
-import platform
-import psutil
-import time
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from languages.get import get_string
-from utils.uptime import get_uptime
-from db.redisdb import redis
+import platform import psutil from datetime import datetime from pyrogram import filters from pyrogram.types import Message from bot import app, START_TIME from languages.get import lang from utils.helpers import get_readable_time
 
-lang = get_string("en")
+_ = lang("en")
 
-@Client.on_message(filters.command("stats"))
-async def stats_command(client: Client, message: Message):
-    cpu = psutil.cpu_percent()
-    ram = psutil.virtual_memory().percent
-    os_info = f"{platform.system()} {platform.release()}"
-    uptime = get_uptime()
+@app.on_message(filters.command("stats")) async def stats_cmd(_, message: Message): uptime = get_readable_time((datetime.now() - START_TIME).seconds) cpu = psutil.cpu_percent() ram = psutil.virtual_memory().percent disk = psutil.disk_usage("/").percent platform_info = platform.system() + " " + platform.release()
 
-    try:
-        ping_start = time.time()
-        await redis.ping()
-        redis_ping = round((time.time() - ping_start) * 1000, 2)
-    except:
-        redis_ping = "ᴏғғʟɪɴᴇ"
+text = _("general.stats_full").format(
+    uptime=uptime,
+    cpu=cpu,
+    ram=ram,
+    disk=disk,
+    platform=platform_info
+)
 
-    stats_text = (
-        f"**{lang['general']['stats']}**\n"
-        f"ᴏꜱ: {os_info}\n"
-        f"ᴜᴘᴛɪᴍᴇ: {uptime}\n"
-        f"ᴄᴘᴜ: {cpu}%\n"
-        f"ʀᴀᴍ: {ram}%\n"
-        f"ʀᴇᴅɪꜱ: {redis_ping} ms"
-    )
-    await message.reply(stats_text)
+await message.reply_text(text)
+
