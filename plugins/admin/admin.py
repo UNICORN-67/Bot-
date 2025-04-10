d66ac87c0466019a0c7f87ea4b8669b8ee15a477
UNICORN-67 <parthtsingh@gmail.com>
2025-04-10 20:55:05 +0530

    Update admin.py

--- plugins/admin/admin.py
+++ plugins/admin/admin.py
@@ -13,19 +13,31 @@ _ = lang("en")
 async def ban_user(, message: Message): 
 user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.ban_member(user.id) await message.reply_text(_("admin.banned"))
 
-@app.on_message(filters.command("unban") & filters.group) async def unban_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.unban_member(user.id) await message.reply_text(_("admin.unbanned"))
+@app.on_message(filters.command("unban") & filters.group) 
+async def unban_user(, message: Message): 
+user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.unban_member(user.id) await message.reply_text(_("admin.unbanned"))
 
-@app.on_message(filters.command("kick") & filters.group) async def kick_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.ban_member(user.id) await message.chat.unban_member(user.id) await message.reply_text(_("admin.kicked"))
+@app.on_message(filters.command("kick") & filters.group) 
+async def kick_user(, message: Message): 
+user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.ban_member(user.id) await message.chat.unban_member(user.id) await message.reply_text(_("admin.kicked"))
 
-@app.on_message(filters.command("gban") & filters.group) async def gban_user(, message: Message): await message.reply_text(("admin.gban"))
+@app.on_message(filters.command("gban") & filters.group) 
+async def gban_user(, message: Message): 
+await message.reply_text(("admin.gban"))
 
-@app.on_message(filters.command("ungban") & filters.group) async def ungban_user(, message: Message): await message.reply_text(("admin.ungban"))
+@app.on_message(filters.command("ungban") & filters.group) 
+async def ungban_user(, message: Message): await message.reply_text(("admin.ungban"))
 
-@app.on_message(filters.command("gpromote") & filters.group) async def gpromote_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.promote_member(user.id, can_manage_chat=True, can_change_info=True, can_invite_users=True, can_delete_messages=True, can_restrict_members=True, can_pin_messages=True, can_promote_members=True) await message.reply_text(_("admin.promoted"))
+@app.on_message(filters.command("gpromote") & filters.group) 
+async def gpromote_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.promote_member(user.id, can_manage_chat=True, can_change_info=True, can_invite_users=True, can_delete_messages=True, can_restrict_members=True, can_pin_messages=True, can_promote_members=True) await message.reply_text(_("admin.promoted"))
 
-@app.on_message(filters.command("gdemote") & filters.group) async def gdemote_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.promote_member(user.id, is_anonymous=False, can_manage_chat=False, can_change_info=False, can_invite_users=False, can_delete_messages=False, can_restrict_members=False, can_pin_messages=False, can_promote_members=False) await message.reply_text(_("admin.demoted"))
+@app.on_message(filters.command("gdemote") & filters.group) 
+async def gdemote_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.promote_member(user.id, is_anonymous=False, can_manage_chat=False, can_change_info=False, can_invite_users=False, can_delete_messages=False, can_restrict_members=False, can_pin_messages=False, can_promote_members=False) await message.reply_text(_("admin.demoted"))
 
-@app.on_message(filters.command("warn") & filters.group) async def warn_user(, message: Message): await message.reply_text(("admin.warned"))
+@app.on_message(filters.command("warn") & filters.group) 
+async def warn_user(, message: Message): 
+await message.reply_text(("admin.warned"))
 
-@app.on_message(filters.command("unwarn") & filters.group) async def unwarn_user(, message: Message): await message.reply_text(("admin.unwarned"))
+@app.on_message(filters.command("unwarn") & filters.group) 
+async def unwarn_user(, message: Message): await message.reply_text(("admin.unwarned"))
 
