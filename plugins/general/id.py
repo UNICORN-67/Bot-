367f329967fdfba6029c7af1f568c1c7b559043e
UNICORN-67 <parthtsingh@gmail.com>
2025-04-11 20:25:29 +0530

    Update id.py

--- plugins/general/id.py
+++ plugins/general/id.py
@@ -1,10 +1,14 @@
 # ID command
 
-from pyrogram import filters from pyrogram.types import Message from bot import app from languages.get import lang
+from pyrogram import filters 
+from pyrogram.types import Message 
+from bot import app 
+from languages.get import lang
 
 _ = lang("en")
 
-@app.on_message(filters.command("id")) async def get_id(_, message: Message): user_id = message.from_user.id if message.from_user else "Unknown" chat_id = message.chat.id reply_id = message.reply_to_message.from_user.id if message.reply_to_message else "N/A"
+@app.on_message(filters.command("id")) 
+async def get_id(_, message: Message): user_id = message.from_user.id if message.from_user else "Unknown" chat_id = message.chat.id reply_id = message.reply_to_message.from_user.id if message.reply_to_message else "N/A"
 
 text = _("general.id_info").format(
     user_id=user_id,
