c9c3933ff54bc9de0ec71ef2905a9a718fc7a460
UNICORN-67 <parthtsingh@gmail.com>
2025-04-11 19:54:52 +0530

    Update admin.py

--- plugins/admin/admin.py
+++ plugins/admin/admin.py
@@ -1,43 +1,83 @@
 # All admin commands: ban, gban, warn, etc.
 
-from pyrogram import filters 
-from pyrogram.types import Message 
-from bot import app 
-from languages.get import lang 
-from utils.helpers import extract_user 
-from pyrogram.enums import ChatMemberStatus
+from pyrogram import filters
+from pyrogram.types import Message
+from bot import app
+from utils.helpers import extract_user
+from utils.logger import log_action
+from db.redisdb import is_toggle_enabled
+from languages.get import get_string
 
-_ = lang("en")
+lang_key = "en"
 
-@app.on_message(filters.command("ban") & filters.group) 
-async def ban_user(, message: Message): 
-user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.ban_member(user.id) await message.reply_text(_("admin.banned"))
+@app.on_message(filters.command(["ban", "unban", "kick", "warn", "unwarn", "gban", "ungban", "gpromote", "gdemote"]))
+async def admin_commands(client, message: Message):
+    if not message.reply_to_message:
+        await message.reply(get_string(lang_key, "reply_user"))
+        return
 
-@app.on_message(filters.command("unban") & filters.group) 
-async def unban_user(, message: Message): 
-user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.unban_member(user.id) await message.reply_text(_("admin.unbanned"))
+    user = await extract_user(message)
+    if not user:
+        await message.reply(get_string(lang_key, "user_not_found"))
+        return
 
-@app.on_message(filters.command("kick") & filters.group) 
-async def kick_user(, message: Message): 
-user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.ban_member(user.id) await message.chat.unban_member(user.id) await message.reply_text(_("admin.kicked"))
+    cmd = message.command[0].lower()
+    chat_id = message.chat.id
 
-@app.on_message(filters.command("gban") & filters.group) 
-async def gban_user(, message: Message): 
-await message.reply_text(("admin.gban"))
+    if cmd == "ban":
+        await app.ban_chat_member(chat_id, user.id)
+        await message.reply(get_string(lang_key, "banned"))
+        await log_action("ban", chat_id, user.id)
 
-@app.on_message(filters.command("ungban") & filters.group) 
-async def ungban_user(, message: Message): await message.reply_text(("admin.ungban"))
+    elif cmd == "unban":
+        await app.unban_chat_member(chat_id, user.id)
+        await message.reply(get_string(lang_key, "unbanned"))
+        await log_action("unban", chat_id, user.id)
 
-@app.on_message(filters.command("gpromote") & filters.group) 
-async def gpromote_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.promote_member(user.id, can_manage_chat=True, can_change_info=True, can_invite_users=True, can_delete_messages=True, can_restrict_members=True, can_pin_messages=True, can_promote_members=True) await message.reply_text(_("admin.promoted"))
+    elif cmd == "kick":
+        await app.ban_chat_member(chat_id, user.id)
+        await app.unban_chat_member(chat_id, user.id)
+        await message.reply(get_string(lang_key, "kicked"))
+        await log_action("kick", chat_id, user.id)
 
-@app.on_message(filters.command("gdemote") & filters.group) 
-async def gdemote_user(, message: Message): user = await extract_user(message) if not user: return await message.reply_text(("admin.no_user")) await message.chat.promote_member(user.id, is_anonymous=False, can_manage_chat=False, can_change_info=False, can_invite_users=False, can_delete_messages=False, can_restrict_members=False, can_pin_messages=False, can_promote_members=False) await message.reply_text(_("admin.demoted"))
+    elif cmd == "warn":
+        await message.reply(get_string(lang_key, "warned"))
+        await log_action("warn", chat_id, user.id)
 
-@app.on_message(filters.command("warn") & filters.group) 
-async def warn_user(, message: Message): 
-await message.reply_text(("admin.warned"))
+    elif cmd == "unwarn":
+        await message.reply(get_string(lang_key, "unwarned"))
+        await log_action("unwarn", chat_id, user.id)
 
-@app.on_message(filters.command("unwarn") & filters.group) 
-async def unwarn_user(, message: Message): await message.reply_text(("admin.unwarned"))
+    elif cmd == "gban":
+        await message.reply(get_string(lang_key, "gbanned"))
+        await log_action("gban", chat_id, user.id)
 
+    elif cmd == "ungban":
+        await message.reply(get_string(lang_key, "ungbanned"))
+        await log_action("ungban", chat_id, user.id)
+
+    elif cmd == "gpromote":
+        await app.promote_chat_member(
+            chat_id, user.id,
+            can_change_info=True,
+            can_delete_messages=True,
+            can_invite_users=True,
+            can_restrict_members=True,
+            can_pin_messages=True,
+            can_promote_members=False,
+        )
+        await message.reply(get_string(lang_key, "promoted"))
+        await log_action("gpromote", chat_id, user.id)
+
+    elif cmd == "gdemote":
+        await app.promote_chat_member(
+            chat_id, user.id,
+            can_change_info=False,
+            can_delete_messages=False,
+            can_invite_users=False,
+            can_restrict_members=False,
+            can_pin_messages=False,
+            can_promote_members=False,
+        )
+        await message.reply(get_string(lang_key, "demoted"))
+        await log_action("gdemote", chat_id, user.id)
