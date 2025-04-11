3b2d0ad50ad7c034adef4a273be650b929510f3d
UNICORN-67 <parthtsingh@gmail.com>
2025-04-11 20:00:35 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,26 +1,83 @@
-from pyrogram.types import User
+pyrogram import filters
+from pyrogram.types import Message
+from bot import app
+from utils.helpers import extract_user
+from utils.logger import log_action
+from db.redisdb import is_toggle_enabled
+from languages.get import get_string
+
+lang_key = "en"
+
+@app.on_message(filters.command(["ban", "unban", "kick", "warn", "unwarn", "gban", "ungban", "gpromote", "gdemote"]))
+async def admin_commands(client, message: Message):
+    if not message.reply_to_message:
+        await message.reply(get_string(lang_key, "reply_user"))
+        return
+
+    user = await extract_user(message)
+    if not user:
+        await message.reply(get_string(lang_key, "user_not_found"))
+        return
+
+    cmd = message.command[0].lower()
+    chat_id = message.chat.id
+
+    if cmd == "ban":
+        await app.ban_chat_member(chat_id, user.id)
+        await message.reply(get_string(lang_key, "banned"))
+        await log_action("ban", chat_id, user.id)
+
+    elif cmd == "unban":
+        await app.unban_chat_member(chat_id, user.id)
+        await message.reply(get_string(lang_key, "unbanned"))
+        await log_action("unban", chat_id, user.id)
+
+    elif cmd == "kick":
+        await app.ban_chat_member(chat_id, user.id)
+        await app.unban_chat_member(chat_id, user.id)
+        await message.reply(get_string(lang_key, "kicked"))
+        await log_action("kick", chat_id, user.id)
+
+    elif cmd == "warn":
+        await message.reply(get_string(lang_key, "warned"))
+        await log_action("warn", chat_id, user.id)
+
+    elif cmd == "unwarn":
+        await message.reply(get_string(lang_key, "unwarned"))
+        await log_action("unwarn", chat_id, user.id)
+
+    elif cmd == "gban":
+        await message.reply(get_string(lang_key, "gbanned"))
+        await log_action("gban", chat_id, user.id)
+
+    elif cmd == "ungban":
+        await message.reply(get_string(lang_key, "ungbanned"))
+        await log_action("ungban", chat_id, user.id)
+
+    elif cmd == "gpromote":
+        await app.promote_chat_member(
+            chat_id, user.id,
+            can_change_info=True,
+            can_delete_messages=True,
+            can_invite_users=True,
+            can_restrict_members=True,
+            can_pin_messages=True,
+            can_promote_members=False,
+        )
+        await message.reply(get_string(lang_key, "promoted"))
+        await log_action("gpromote", chat_id, user.id)
+
+    elif cmd == "gdemote":
+        await app.promote_chat_member(
+            chat_id, user.id,
+            can_change_info=False,
+            can_delete_messages=False,
+            can_invite_users=False,
+            can_restrict_members=False,
+            can_pin_messages=False,
+            can_promote_members=False,
+        )
+        await message.reply(get_string(lang_key, "demoted"))
+        await log_action("gdemote", chat_id, user.id)
 
 
-def mention_user(user: User) -> str:
-    """Mention a user with markdown link."""
-    return f"[{user.first_name}](tg://user?id={user.id})"
-
-
-def get_user_id(message):
-    """Get user ID from reply or command argument."""
-    if message.reply_to_message:
-        return message.reply_to_message.from_user.id
-    elif len(message.command) > 1:
-        if message.command[1].isdigit():
-            return int(message.command[1])
-    return None
-
-
-def get_chat_id(message):
-    """Get chat ID from message."""
-    return message.chat.id if message.chat else None
-
-
-def get_command_arg(message):
-    """Get text argument after command."""
-    return message.text.split(None, 1)[1] if len(message.command) > 1 else None
