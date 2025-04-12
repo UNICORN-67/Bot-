03ebe9bf0d051c56eadfe6e141625c52a09efa98
UNICORN-67 <parthtsingh@gmail.com>
2025-04-12 12:37:12 +0530

    Update nsfw.py

--- plugins/security/nsfw.py
+++ plugins/security/nsfw.py
@@ -2,7 +2,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from nsfw.detector import detect_text_nsfw, detect_media_nsfw
+from nsfw.detector import detect_text_nsfw, detect_image_nsfw, detect_video_nsfw
 from db.redisdb import get_toggle
 from languages.get import get_string
 from utils.helpers import get_user_id
@@ -20,17 +20,30 @@ async def nsfw_protection(client: Client, message: Message):
     if message.from_user and message.from_user.id in OWNER_ID:
         return
 
-    media = message.photo or message.video or message.animation or message.sticker
-    if media:
-        try:
-            is_nsfw = await detect_media_nsfw(client, message)
-        except Exception:
-            return
-        if is_nsfw:
-            await message.delete()
-            await message.reply_text(_("nsfw_detected"))
-        return
-
+    # Text detection
     if message.text and detect_text_nsfw(message.text):
         await message.delete()
         await message.reply_text(_("nsfw_detected"))
+        return
+
+    # Image detection (photo or sticker)
+    if message.photo or (message.sticker and not message.sticker.is_animated):
+        try:
+            is_nsfw = await detect_image_nsfw(client, message)
+            if is_nsfw:
+                await message.delete()
+                await message.reply_text(_("nsfw_detected"))
+        except Exception as e:
+            print("Image NSFW detection error:", e)
+        return
+
+    # Video or animated sticker detection
+    if message.video or message.animation or (message.sticker and message.sticker.is_animated):
+        try:
+            is_nsfw = await detect_video_nsfw(client, message)
+            if is_nsfw:
+                await message.delete()
+                await message.reply_text(_("nsfw_detected"))
+        except Exception as e:
+            print("Video NSFW detection error:", e)
+        return
